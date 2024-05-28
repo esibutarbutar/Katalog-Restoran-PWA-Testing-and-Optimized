6ef42eac3a6e3744939402284734975f469de369
esibutarbutar <211112609@students.mikroskil.ac.id>
2024-05-28 08:45:07 +0700

    Edit

--- webpack.common.js
+++ webpack.common.js
@@ -101,7 +101,10 @@ module.exports = {
         },
       ],
     }),
-    new BundleAnalyzerPlugin(),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+    }),
 
   ],
 };
